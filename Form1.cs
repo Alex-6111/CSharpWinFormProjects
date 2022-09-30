e6189e1356f4f1402051fba71febad853db9d904
Admin <obekas777@gmail.com>
2022-09-30 20:44:37 +0300

    Complete

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,26 @@ namespace CSharpWinFormProjects
         public Form1()
         {
             InitializeComponent();
+            this.Shown += GuessNumber;
+        }
+
+        private void GuessNumber(object sender, EventArgs e)
+        {
+            DialogResult result;
+            int numDialog = 1;
+
+            while (true)
+            {
+                result = MessageBox.Show($"{new Random().Next(1, 2000)}", "Your number", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if(result == DialogResult.Yes)
+                {
+                    MessageBox.Show($"Number of requests {numDialog}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    numDialog = 0;
+                    result = MessageBox.Show($"Continue?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.No) this.Close();
+                }
+                numDialog++;
+            }
         }
     }
 }
