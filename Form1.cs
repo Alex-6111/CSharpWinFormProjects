80b28cb97fadbdff8a5117a25b1fc0c6e060d17c
Admin <obekas777@gmail.com>
2022-10-03 20:35:55 +0300

    Complete

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,42 @@ namespace CSharpWinFormProjects
         public Form1()
         {
             InitializeComponent();
+            this.MouseMove += MousePosition;
+            this.MouseClick += PointData;
+
+        }
+
+        private void PointData(object sender, MouseEventArgs e)
+        {
+            string text = "";
+            if(e.Button == MouseButtons.Left)
+            {
+                if(ModifierKeys == Keys.Control)
+                {
+                    Close();
+                }
+                if((e.X < 10 || e.X > ClientSize.Width - 10) || (e.Y < 10 || e.Y > ClientSize.Height - 10))
+                {
+                    text = "The mouse is outside the rectangle";
+                }
+                else if((e.X == 10 || e.X == ClientSize.Width - 10) || (e.Y == 10 || e.Y == ClientSize.Height - 10))
+                {
+                    text = "The mouse is on the border of the rectangle";
+                }
+                else
+                {
+                    text = "The mouse is the rectangle";
+                }
+            }
+            if(e.Button == MouseButtons.Right)
+            {
+                Text = $"Window size Width -> {ClientSize.Width}, Height -> {ClientSize.Height}";
+                Thread.Sleep(2000);
+            }
+        }
+        private void MousePosition(object sender, MouseEventArgs e)
+        {
+            Text = $"X ->  {e.X} - Y {e.Y}";
         }
     }
 }
