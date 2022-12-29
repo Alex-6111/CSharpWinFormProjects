93cbef1f5dbb736a5c4b6c5934a74f59827cd61c
Oleksandr Bekas <lxbekas@gmail.com>
2022-12-29 18:52:34 +0200

    Complete

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,32 @@ namespace CSharpWinFormProjects
         public Form1()
         {
             InitializeComponent();
+            this.Paint += Form1_Paint1;
+        }
+
+        private void Form1_Paint1(object sender, PaintEventArgs e)
+        {
+            bool isBlack = false;
+            Pen pen = new Pen(Color.Black);
+            Brush brush = new SolidBrush(Color.Black);
+            Rectangle rect = new Rectangle();
+            Graphics gfx = e.Graphics;
+            for (int i = 1; i <= 8; i++)
+            {
+                for (int j = 1; j <= 8; j++)
+                {
+                    if (isBlack)
+                    {
+                        rect = new Rectangle(i * 45, j * 45, 45, 45);
+                        gfx.DrawRectangle(pen, rect);
+                        gfx.FillRectangle(brush, rect);
+                        isBlack = false;
+                    }
+                    else
+                        isBlack = true;
+                }
+                isBlack = !isBlack;
+            }
         }
     }
 }
